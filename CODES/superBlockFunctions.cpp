6d77d645f21beab1749fac73f289c1387d10f357
Aniket K. Singh <33181885+singhaniket98@users.noreply.github.com>
2020-02-12 01:35:30 -0500

    Update superBlockFunctions.cpp

--- CODES/superBlockFunctions.cpp
+++ CODES/superBlockFunctions.cpp
@@ -13,6 +13,7 @@
 #include"superblockFunctions.h"
 #include"superblock.h"
 #include"ext2File.h"
+#include"blockGroupDescriptor.h"
 #include<iostream>
 using namespace std;
 
@@ -42,9 +43,9 @@ int displaySuperBlock(struct ext2File* ext2){
   cout<<"Blocks per group : "<<std::dec<< ext2->superblock.s_blocks_per_group<<"\n";
   cout<<"Fragments per group : "<<std::dec<< ext2->superblock.s_frags_per_group<<"\n";
   cout<<"Inodes per group : "<<std::dec<< ext2->superblock.s_inodes_per_group<<"\n";
-  cout<<"Last mount time : "<<std::dec<< ext2->superblock.s_last_mounted<<"\n";
+  cout<<"Last mount time : "<<std::dec<< ext2->superblock.s_mtime<<"\n";
   cout<<"Last write time : "<<std::dec<< ext2->superblock.s_wtime<<"\n";
-  cout<<"Mount count : "<<std::dec<< ext2->superblock.s_mtime<<"\n";
+  cout<<"Mount count : "<<std::dec<< ext2->superblock.s_mnt_count<<"\n";
   cout<<"Max mount count : "<<std::dec<< ext2->superblock.s_max_mnt_count<<"\n";
   cout<<"Magic Number : "<<std::hex<< ext2->superblock.s_magic<<"\n";
   cout<<"State : "<<std::dec<< ext2->superblock.s_state<<"\n";
@@ -56,22 +57,36 @@ int displaySuperBlock(struct ext2File* ext2){
   cout<<"Default reserve UID : "<<std::dec<< ext2->superblock.s_def_resuid<<"\n";
   cout<<"Default reserve GID : "<<std::dec<< ext2->superblock.s_def_resgid<<"\n";
   cout<<"First Inode Number : "<<std::dec<< ext2->superblock.s_first_ino<<"\n";
-  cout<<"Inode Size : "<<std::dec<< ext2->superblock.s_inodes_count;
-  cout<<"Block group Number : "<<std::dec<< ext2->superblock.s_inodes_count;
-  cout<<"Feature Compatibility bits : "<<std::dec<< ext2->superblock.s_inodes_count;
-  cout<<"Feature incompatibility bits : "<<std::dec<< ext2->superblock.s_inodes_count;
-  cout<<"Feature read/only Compatibility bits : "<<std::dec<< ext2->superblock.s_inodes_count;
-  cout<<"UUID : "<<std::dec<< ext2->superblock.s_inodes_count;
-  cout<<"Volume Name : "<<std::dec<< ext2->superblock.s_inodes_count;
-  cout<<"Last Mount : "<<std::dec<< ext2->superblock.s_inodes_count;
-  cout<<"Algorithm bitmap : "<<std::dec<< ext2->superblock.s_inodes_count;
-  cout<<"Number of blocks to preallocate : "<<std::dec<< ext2->superblock.s_inodes_count;
-  cout<<"Number of blocks to preallocate for directories : "<<std::dec<< ext2->superblock.s_inodes_count;
-  cout<<"Journal UUID : "<<std::dec<< ext2->superblock.s_inodes_count;
-  cout<<"Journal inode Number : "<<std::dec<< ext2->superblock.s_inodes_count;
-  cout<<"Journal Device Number : "<<std::dec<< ext2->superblock.s_inodes_count;
-  cout<<"Journal Last orphan inode Number : "<<std::dec<< ext2->superblock.s_inodes_count;
-  cout<<"First Meta Block Group : "<<std::dec<< ext2->superblock.s_inodes_count;
+  cout<<"Inode Size : "<<std::dec<< ext2->superblock.s_inode_size<<"\n";
+  cout<<"Block group Number : "<<std::dec<< ext2->superblock.s_block_group_nr<<"\n";
+  cout<<"Feature Compatibility bits : "<<std::dec<< ext2->superblock.s_feature_compat<<"\n";
+  cout<<"Feature incompatibility bits : "<<std::dec<< ext2->superblock.s_feature_incompat<<"\n";
+  cout<<"Feature read/only Compatibility bits : "<<std::dec<< ext2->superblock.s_feature_ro_compat<<"\n";
+  cout<<"UUID : ";
+  for(uint8_t x: ext2->superblock.s_uuid){
+    cout<<std::hex<<(int)x;
+  }
+  cout<<"\n";
+  cout<<"Volume Name : ";
+  for(uint8_t x: ext2->superblock.s_volume_name){
+    cout<<std::hex<<(int)x;
+  }
+  cout<<"\n";
+  cout<<"Last Mount : "<<std::dec<< ext2->superblock.s_last_mounted<<"\n";
+  cout<<"Algorithm bitmap : "<<std::dec<< ext2->superblock.s_algo_bitmap<<"\n";
+  cout<<"Number of blocks to preallocate : "<< (int)ext2->superblock.s_prealloc_blocks<<"\n";
+  cout<<"Number of blocks to preallocate for directories : "<<(int) ext2->superblock.s_prealloc_dir_blocks<<"\n";
+  cout<<"Journal UUID : ";
+  for(uint8_t x: ext2->superblock.s_journal_uuid){
+    cout<<std::hex<<(int)x;
+  }
+  cout<<"\n";
+  cout<<"Journal inode Number : "<<std::dec<< ext2->superblock.s_journal_inum<<"\n";
+  cout<<"Journal Device Number : "<<std::dec<< ext2->superblock.s_journal_dev<<"\n";
+  cout<<"Journal Last orphan inode Number : "<<std::dec<< ext2->superblock.s_last_orphan<<"\n";
+  cout<<"Default hash version : "<<(int) ext2->superblock.s_def_hash_version<<"\n";
+  cout<<"Default mount option bitmap : "<<std::hex<< ext2->superblock.s_default_mount_opts<<"\n";
+  cout<<"First Meta Block Group : "<<std::dec<< ext2->superblock.s_first_meta_bg<<"\n";
 
 }
 
