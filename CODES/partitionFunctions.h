0437e650dd2c7b950f0f830c45c59e2994e516a9
Nirajan Acharya <47442286+nacharya01@users.noreply.github.com>
2020-03-29 23:39:49 -0400

    added some functions

--- CODES/partitionFunctions.h
+++ CODES/partitionFunctions.h
@@ -7,10 +7,10 @@
 #include"partitionData.h"
 #include"mbr.h"
 
-struct mbrSector * mbrOpen(struct vdifile *file,struct partitionEntry);
-void mbrClose(struct mbrSector *f);
+struct partitionFile * partitionFileOpen(struct vdifile *file,struct partitionEntry);
+void partitionClose(struct partitionFile *partition);
+int partitionSeek(struct partitionFile *f,int offset,int anchor,struct partitionEntry partitionInfo[]);
 int mbrRead(struct vdifile *f, struct mbrSector& buf,int count);
-int mbrWrite(struct mbrSector *f,void * buf, int count);
 int mbrSeek(struct vdifile *f,int offset,int anchor);
 void displayPartitionInfo(struct mbrSector mbrData);
 void displaySuperBlock(struct vdifile *f,struct dataBlock data,struct mbrSector mbrData);
