dc2458e34dd5a1c68e87262413034fa6a4c91953
Nirajan Acharya <47442286+nacharya01@users.noreply.github.com>
2020-03-29 23:37:55 -0400

    added partitionFile

--- CODES/mbr.h
+++ CODES/mbr.h
@@ -1,6 +1,7 @@
 #ifndef MBR
 #define MBR
 #include"partitionData.h"
+#include"vdifile.h"
 #pragma pack(1)
 const unsigned short BOOT_SECTOR_MAGIC = 0xaa55;
 
@@ -10,4 +11,8 @@ struct mbrSector{
   uint16_t bootSignature;
 };
 
+struct __attribute__((packed)) partitionFile{
+  struct vdifile * file;
+  struct partitionEntry partitionEntryInfo[4];
+};
 #endif
