81653e9aa162938d1cd9dc2e2ff5b10048fa4884
Nirajan Acharya <47442286+nacharya01@users.noreply.github.com>
2020-04-23 16:02:47 -0400

    final

--- CODES/superblockFunctions.h
+++ CODES/superblockFunctions.h
@@ -5,7 +5,7 @@
 #include"mbr.h"
 
 int readSuperBlock(struct ext2File * ext2,uint32_t blockNumber,struct vdifile * file,struct mbrSector mbr,int translationMapData[] );
-void writeSuperBlock(struct ext2File* ext2, uint32_t blockNumber,struct vdifile * file,struct mbrSector mbr,struct superBlock & buffer,int translationMapData[]);
+void writeSuperBlock(struct ext2File* ext2,struct vdifile * file,struct mbrSector mbr,struct superBlock & buffer,int translationMapData[]);
 int displaySuperBlock(struct ext2File* ext2);
 int actualPage(int virtualAddress, struct vdifile *file, int translationMapData[]);
 #endif
